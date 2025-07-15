871e54ec15d24ed89ebb9f8be4471da5534dacf9
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-07-15 15:22:24 -0600

    Added Final game result Print Added a function that prints the end game resul upon reaching round limit

--- scriptv2.js
+++ scriptv2.js
@@ -20,23 +20,42 @@ const para = document.createElement("p");
 
   playerOptions.addEventListener("click", (event) => {
     let randomNum = Math.floor(Math.random() * 100);
-    console.log(randomNum)
-    let cpuChoice = cpuPicker(randomNum)
-    console.log(cpuChoice)
+    console.log(randomNum);
+    let cpuChoice = cpuPicker(randomNum);
+    console.log(cpuChoice);
 
     if (event.target.id === cpuChoice ) {
-      para.textContent = "the round is a draw"
-      output.appendChild(para)
+      para.textContent = "the round is a draw";
+      output.appendChild(para);
     }
     else if (event.target.id == "P" && cpuChoice == "R" ||
              event.target.id == "S" && cpuChoice == "P" ||
              event.target. id == "R" && cpuChoice == "S") {
-      para.textContent = "Player wins!"
-      output.appendChild(para)
+      para.textContent = "Player wins!";
+      output.appendChild(para);
+      playerWin++;
+    console.log(`The score is ${cpuWin} to ${playerWin}`)
+
     }
     else {
-      para.textContent = "CPU wins!"
-      output.appendChild(para)
+      para.textContent = "CPU wins!";
+      output.appendChild(para);
+      cpuWin++;
     }
+    console.log(`The score is ${cpuWin} to ${playerWin}`)
+
   })
 
+const endGameResult = document.querySelector("#gameEnd");
+const announce = document.createElement("h1");
+
+function gameOver(cpuWin, playerWin) {
+  if (cpuWin == 5) {
+    announce.textContent = `CPU wins ${cpuWin} to ${playerWin}`
+    endGameResult.appendChild(announce)
+  }
+  else if (playerWin == 5) {
+    announce.textContent = `Player wins ${playerWin} to ${cpuWin}`
+    endGameResult.appendChild(announce)
+  }
+}
